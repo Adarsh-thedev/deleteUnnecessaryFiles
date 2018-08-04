a15312f66f355186914dc559ed51f3754f9484c1
Adarsh-thedev <adarshpittu@gmail.com>
2018-08-04 14:02:51 +0530

    Reduced Some Lines of Code

--- deleteUnnecessaryFiles.py
+++ deleteUnnecessaryFiles.py
@@ -1,12 +1,13 @@
 import os
 
-folder1Path=(r"")  #Write path of original folder inside the "" after r
+folder1Path=(r"E:\DSLR Images")  #Write path of original folder inside the "" after r
 folder1Files=os.listdir(folder1Path)
 
-folder2Path=(r"")  #Write path of the folder from which you want files to be deleted inside the "" after r
+folder2Path=(r"E:\Random")  #Write path of the folder from which you want files to be deleted inside the "" after r
 folder2Files=os.listdir(folder2Path) 
 
 for files in folder1Files:
 	if files in folder2Files:
 		commonFiles=os.path.join(folder2Path,files)
 		os.remove(commonFiles)
+print("Successful! Please Check Your Folder :)")
